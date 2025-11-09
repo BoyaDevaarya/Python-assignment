4f5b21258f0a16b6f8f944dfb7072a556eb50205
Srihariprasad-codes421 <srihariprasads31@gmail.com>
2025-11-10 02:12:14 +0530

    devaarya

---
+++ hello.py
@@ -0,0 +1,2 @@
+print("hello world")
+str=input("thank you:")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+print("Example 2:")
+print("Input: nums =", nums2, ", target =", target2)
+print("Output:", twoSum(nums2, target2))
+print("-" * 40)
+
+# -------------------------------
+# Example 3
+nums3 = [3, 3]
+target3 = 6
